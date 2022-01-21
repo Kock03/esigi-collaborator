3a7f216071b40782b553049691d740cbb0525ad6
davikrig-ha <daviluia2@gmail.com>
2022-01-21 11:33:44 -0300

    alterations layout

--- webapp/src/app/modules/job/job-create/job-create.component.ts
+++ webapp/src/app/modules/job/job-create/job-create.component.ts
@@ -1,12 +1,18 @@
 import { HttpClient } from '@angular/common/http';
-import { Component, Inject, OnInit, ViewEncapsulation } from '@angular/core';
-import { FormBuilder, FormGroup, Validators } from '@angular/forms';
+import { Component, EventEmitter, Inject, Input, OnInit, Output, ViewChild, ViewEncapsulation } from '@angular/core';
+import { FormArray, FormBuilder, FormGroup, Validators } from '@angular/forms';
 import {
   MatDialog,
   MatDialogRef,
   MAT_DIALOG_DATA,
 } from '@angular/material/dialog';
+import { MatTable } from '@angular/material/table';
 import { JobProvider } from 'src/providers/job.provider';
+
+export interface Knowledge {
+  name: string,
+  yearsExperience: number,
+}
 
 @Component({
   selector: 'app-job-create',
@@ -15,11 +21,33 @@ import { JobProvider } from 'src/providers/job.provider';
   encapsulation: ViewEncapsulation.None,
 })
 export class JobCreateComponent implements OnInit {
+  @ViewChild('knowledgeForm') knowledgeForm!: any;
+  @ViewChild('knowledgeTable') knowledgeTable!: MatTable<any>;
+
+  displayedColumns: string[] = ['name', 'yearsExperience', 'icon'];
+
+
+  get knowledgeArray() {
+    return this.jobForm.controls['Knowledges'] as FormArray;
+  }
+  
   jobForm!: FormGroup;
   step: number = 1;
   selectedIndex: number = 0;
   disable = false;
   checked = false;
+ 
+  index: any = null;
+  Knowledge: any;
+
+  knowledge: Knowledge[] = [
+    {
+      name: 'Java',
+      yearsExperience: 1,
+    },
+  ];
+
+
 
   constructor(
     private fb: FormBuilder,
@@ -53,20 +81,21 @@ export class JobCreateComponent implements OnInit {
       openingDate: ['', Validators.required],
       schooling: [1, Validators.required],
       collaboratorActivities: ['a', Validators.required],
-      knowledge: ['a', Validators.required],
-      yearsExperience: [1, Validators.required],
       skills: ['a', Validators.required],
       attitudes: ['a', Validators.required],
       Languages: this.fb.group({
         languageName: ['Russo', Validators.required],
         degreeOfInfluence: [1, Validators.required],
       }),
+      Knowledges: this.fb.array([])
     });
+
   }
 
   openDialog(): void {
     const dialogRef = this.dialog.open(JobDialogSkill, {
       width: '450px',
+      height: '200px',
     });
 
     dialogRef.afterClosed().subscribe((result) => {
@@ -99,16 +128,56 @@ export class JobCreateComponent implements OnInit {
       console.log('ERROR 132' + error);
     }
   }
+
+  deleteKnowledge(index: number) {
+    this.knowledgeArray.removeAt(index);
+  }
 }
 
 @Component({
   selector: 'job-dialog-skill',
   templateUrl: 'job-dialog-skill.html',
 })
-export class JobDialogSkill {
-  constructor(private dialogRef: MatDialogRef<JobDialogSkill>) {}
+export class JobDialogSkill implements OnInit {
+  @Input('form') jobForm!: FormGroup;
+  @Output('onChange') onChange: EventEmitter<any> = new EventEmitter();
+  @ViewChild('knowledgeTable') knowledgeTable!: MatTable<any>;
+  
+
+  knowledgeForm!: FormGroup;  
+
+  knowledge: Knowledge[] = [
+    {
+      name: 'Java',
+      yearsExperience: 1,
+    },
+  ];
+
+  get knowledgeArray() {
+    return this.jobForm.controls['Knowledges'] as FormArray;
+  }
+
+
+  constructor(private dialogRef: MatDialogRef<JobDialogSkill>, private fb: FormBuilder) {}
+
+  ngOnInit(): void {
+    this.initForm();
+  }
 
   onNoClick(): void {
     this.dialogRef.close();
+  }
+
+initForm(): void {
+  this.knowledgeForm = this.fb.group({
+    name: ['', Validators.required],
+    yearsExperience: [1, Validators.required],
+  })}
+
+  saveKnowledge() {
+    const data = this.knowledgeForm.getRawValue();
+    this.knowledgeArray.insert(0, this.fb.group(data));
+    this.knowledgeTable.renderRows();
+    this.knowledgeForm.reset();
   }
 }
