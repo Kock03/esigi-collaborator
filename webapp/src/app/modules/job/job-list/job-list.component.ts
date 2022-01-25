27abeeed3ba3a8de529aad80a35c83a81bbec856
davikrig-ha <daviluia2@gmail.com>
2022-01-25 11:48:56 -0300

    add

--- webapp/src/app/modules/job/job-list/job-list.component.ts
+++ webapp/src/app/modules/job/job-list/job-list.component.ts
@@ -1,12 +1,24 @@
-import { Component, OnInit, ViewEncapsulation } from '@angular/core';
+import {
+  Component,
+  ElementRef,
+  OnInit,
+  ViewChild,
+  ViewEncapsulation,
+} from '@angular/core';
 import { Router } from '@angular/router';
+import {
+  fromEvent,
+  takeUntil,
+  debounceTime,
+  distinctUntilChanged,
+  Subject,
+} from 'rxjs';
 import { JobProvider } from 'src/providers/job.provider';
 
 export interface Job {
   jobName: string;
   client: string;
   requester: string;
-
 }
 
 @Component({
@@ -16,13 +28,20 @@ export interface Job {
   encapsulation: ViewEncapsulation.None,
 })
 export class JobListComponent implements OnInit {
+  // @ViewChild('filter', { static: true }) filter!: ElementRef;
+  // private _unsubscribeAll: Subject<any>;
   displayedJob: string[] = ['jobName', 'client', 'requester'];
   jobs!: Job[];
+  // filteredJobList!: Job[];
 
-  constructor(private router: Router, private jobProvider: JobProvider) {}
+  constructor(private router: Router, private jobProvider: JobProvider) {
+    // this._unsubscribeAll = new Subject();
+  }
 
   ngOnInit(): void {
-
+    this.getJobList();
+    // this.initFilter();
+    // this.filteredJobList = this.jobs;
   }
 
   createJob() {
@@ -36,4 +55,33 @@ export class JobListComponent implements OnInit {
       console.error(error);
     }
   }
+
+  // initFilter() {
+  //   fromEvent(this.filter.nativeElement, 'keyup')
+  //     .pipe(
+  //       takeUntil(this._unsubscribeAll),
+
+  //       debounceTime(200),
+
+  //       distinctUntilChanged()
+  //     )
+
+  //     .subscribe((res) => {
+  //       this.filteredJobList = this.jobs.filter(
+  //         (job) =>
+  //           job.jobName
+
+  //             .toLowerCase()
+
+  //             .startsWith(this.filter.nativeElement.value) ||
+  //           job.jobName
+
+  //             .toUpperCase()
+
+  //             .startsWith(this.filter.nativeElement.value) ||
+  //           job.jobName.endsWith(this.filter.nativeElement.value) ||
+  //           job.jobName.toUpperCase().endsWith(this.filter.nativeElement.value)
+  //       );
+  //     });
+  // }
 }
