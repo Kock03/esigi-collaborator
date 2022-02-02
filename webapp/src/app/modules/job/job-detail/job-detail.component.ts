bd8d3cc07cb1419674a1bb1adc4097cccac9e2d6
davikrig-ha <daviluia2@gmail.com>
2022-02-02 16:35:30 -0300

    add

--- webapp/src/app/modules/job/job-detail/job-detail.component.ts
+++ webapp/src/app/modules/job/job-detail/job-detail.component.ts
@@ -1,21 +1,31 @@
 import { Component, OnInit } from '@angular/core';
-import { ActivatedRoute } from '@angular/router';
+import { ActivatedRoute, Router } from '@angular/router';
 import { JobProvider } from 'src/providers/job.provider';
+import { Location } from "@angular/common";
+
+export interface Job {
+  jobName: string;
+}
 
 @Component({
   selector: 'app-job-detail',
   templateUrl: './job-detail.component.html',
-  styleUrls: ['./job-detail.component.scss']
+  styleUrls: ['./job-detail.component.scss'],
 })
 export class JobDetailComponent implements OnInit {
- 
+  job: any;
 
-
-
-  constructor(   private route: ActivatedRoute, private jobProvider: JobProvider) { }
+  constructor(
+    private router: Router,
+    private route: ActivatedRoute,
+    private _location: Location
+  ) {}
 
   ngOnInit(): void {
+    const state: any = this._location.getState();
+    this.job = state["job"]
+    console.log("🚀 ~ file: job-detail.component.ts ~ line 25 ~ JobDetailComponent ~ ngOnInit ~ state", this.job)
   }
 
-
+ 
 }
