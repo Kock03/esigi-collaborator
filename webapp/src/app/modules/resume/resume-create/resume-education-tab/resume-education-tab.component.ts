849366a567e39e5587caaee72df860e06c2a790f
davikrig-ha <daviluia2@gmail.com>
2022-02-17 08:16:31 -0300

    add

--- webapp/src/app/modules/resume/resume-create/resume-education-tab/resume-education-tab.component.ts
+++ webapp/src/app/modules/resume/resume-create/resume-education-tab/resume-education-tab.component.ts
@@ -73,7 +73,7 @@ export class ResumeEducationTabComponent implements OnInit {
     'situation',
     'course',
     'institution',
-    'iconAdd',
+    'icon'
   ];
 
   displayedLanguage: string[] = ['language', 'fluency', 'icon'];
