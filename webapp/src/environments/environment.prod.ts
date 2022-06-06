14011123450bc94a41f8492638d59ad2875893a7
davikrig-ha <daviluia2@gmail.com>
2022-06-06 10:29:42 -0300

    prod type

--- webapp/src/environments/environment.prod.ts
+++ webapp/src/environments/environment.prod.ts
@@ -1,3 +1,11 @@
 export const environment = {
   production: true,
+
+  COLLABORATOR_MS: '192.168.8.16:3501/api/v1/',
+  RESUME_MS: '192.168.8.16:3502/api/v1/',
+  JOBS_MS: '192.168.8.16:3503/api/v1/',
+  SETTING_MS: '192.168.8.16:3504/api/v1/',
+
+  message: '',
+  protocol: 'http',
 };
