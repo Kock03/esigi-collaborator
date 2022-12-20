bb10ae766d7d7f242076eab35843fd1fb9893930
VINICIUS KOCK <vinikk03@gmail.com>
2022-12-20 09:32:12 -0300

    environment

--- webapp/src/environments/environment.prod.ts
+++ webapp/src/environments/environment.prod.ts
@@ -1,17 +1,17 @@
 export const environment = {
   production: true,
 
-  COLLABORATOR_MS: '192.168.8.184:3501/api/v1/',
-  RESUME_MS: '192.168.8.184:3502/api/v1/',
-  JOBS_MS: '192.168.8.184:3503/api/v1/',
-  SETTING_MS: '192.168.8.184:3504/api/v1/',
-  CUSTOMER_MS: '192.168.8.184:3506/api/v1/',
-  CONFIG_MS: '192.168.8.184:3508/api/v1/registers/',
+  COLLABORATOR_MS: '44.205.159.254:3501/api/v1/',
+  RESUME_MS: '44.205.159.254:3502/api/v1/',
+  JOBS_MS: '44.205.159.254:3503/api/v1/',
+  SETTING_MS: '44.205.159.254:3504/api/v1/',
+  CUSTOMER_MS: '44.205.159.254:3506/api/v1/',
+  CONFIG_MS: '44.205.159.254:3508/api/v1/registers/',
 
-  AUTH_SERVICE_MS: '192.168.8.184:3500/api/v1/',
-  PROJECT_MS: '192.168.8.184:3505/api/v1/',
-  portal: 'http://192.168.8.184:3406/login',
-  port: '192.168.8.184:',
+  AUTH_SERVICE_MS: '44.205.159.254:3500/api/v1/',
+  PROJECT_MS: '44.205.159.254:3505/api/v1/',
+  portal: 'http://44.205.159.254:3406/login',
+  port: '44.205.159.254:',
   message: '',
   protocol: 'http',
 };
