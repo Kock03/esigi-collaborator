379e1e34e92e0a032f754eb44ff6d355d27be287
Kock03 <vinikk03@gmail.com>
2023-01-16 15:23:32 -0300

    arrumando portas

--- webapp/src/environments/environment.ts
+++ webapp/src/environments/environment.ts
@@ -5,14 +5,14 @@
 export const environment = {
   production: false,
   COLLABORATOR_MS: 'https://esigi.envolti.com.br/coll/api/v1/',
-  RESUME_MS: 'https://esigi.envolti.com.br:3502/resume/api/v1/',
-  JOBS_MS: 'https://esigi.envolti.com.br:3503/jobs/api/v1/',
-  SETTING_MS: 'https://esigi.envolti.com.br:3504/setting/api/v1/',
-  CONFIG_MS: 'https://esigi.envolti.com.br:3508/config/api/v1/registers/',
+  RESUME_MS: 'https://esigi.envolti.com.br/resume/api/v1/',
+  JOBS_MS: 'https://esigi.envolti.com.br/jobs/api/v1/',
+  SETTING_MS: 'https://esigi.envolti.com.br/setting/api/v1/',
+  CONFIG_MS: 'https://esigi.envolti.com.br/config/api/v1/registers/',
 
-  AUTH_SERVICE_MS: 'https://esigi.envolti.com.br:3500/auth/api/v1/',
-  CUSTOMER_MS: 'https://esigi.envolti.com.br:3506/customer/api/v1/',
-  PROJECT_MS: 'https://esigi.envolti.com.br:3505/projects/api/v1/',
+  AUTH_SERVICE_MS: 'https://esigi.envolti.com.br/auth/api/v1/',
+  CUSTOMER_MS: 'https://esigi.envolti.com.br/customer/api/v1/',
+  PROJECT_MS: 'https://esigi.envolti.com.br/projects/api/v1/',
   portal: 'https://aws-amplify.d3tee2p1a2jxch.amplifyapp.com/login',
   port: 'https://esigi.envolti.com.br:',
 
