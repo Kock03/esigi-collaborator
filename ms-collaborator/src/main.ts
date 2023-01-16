23dbf036afe9d1a963f4f8f9ae96c5d09b60ee8f
Kock03 <vinikk03@gmail.com>
2023-01-16 14:46:40 -0300

    cors error

--- ms-collaborator/src/main.ts
+++ ms-collaborator/src/main.ts
@@ -23,24 +23,6 @@ async function bootstrap() {
   );
 
   app.enableCors();
-  // await app.listen(3501);
-  app.use(function (req, res, next) {
-
-    // Website you wish to allow to connect
-    res.setHeader('Access-Control-Allow-Origin', 'https://esigi.envolti.com.br/');
-
-    // Request methods you wish to allow
-    res.setHeader('Access-Control-Allow-Methods', 'GET, POST, OPTIONS, PUT, PATCH, DELETE');
-
-    // Request headers you wish to allow
-    res.setHeader('Access-Control-Allow-Headers', 'X-Requested-With,content-type');
-
-    // Set to true if you need the website to include cookies in the requests sent
-    // to the API (e.g. in case you use sessions)
-    res.setHeader('Access-Control-Allow-Credentials', true);
-
-    // Pass to next layer of middleware
-    next();
-});
+  await app.listen(3501);
 }
 bootstrap();
