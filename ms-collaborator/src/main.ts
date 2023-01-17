5eb168c98042eb23e29673fb0577ff4716e531e8
Kock03 <vinikk03@gmail.com>
2023-01-17 11:57:10 -0300

    testing cors parameter

--- ms-collaborator/src/main.ts
+++ ms-collaborator/src/main.ts
@@ -25,5 +25,12 @@ async function bootstrap() {
 
   app.enableCors();
   // await app.listen(3501);
+  const cors = require('cors');
+  app.use((req, res, next) => {
+    res.headers("Access-Control-Allow-Origin", "*");
+    res.header("Access-Control-Allow-Methods", 'GET, POST, PUT, DELETE');
+    app.use(cors());
+    next();
+  })
 }
 bootstrap();
