b10fae16d1528cf4cea12d3b5c0dc120c2fa090e
Kock03 <vinikk03@gmail.com>
2023-01-17 13:40:10 -0300

    cors problem trying to solve

--- ms-collaborator/src/main.ts
+++ ms-collaborator/src/main.ts
@@ -27,10 +27,21 @@ async function bootstrap() {
   // await app.listen(3501);
   const cors = require('cors');
   app.use((req, res, next) => {
-    res.headers("Access-Control-Allow-Origin", "*");
-    res.header("Access-Control-Allow-Methods", 'GET, POST, PUT, DELETE');
-    app.use(cors());
-    next();
+   // Website you wish to allow to connect
+   res.setHeader('Access-Control-Allow-Origin', 'https://aws-amplify.d26nj5ra7tr3nr.amplifyapp.com');
+
+   // Request methods you wish to allow
+   res.setHeader('Access-Control-Allow-Methods', 'GET, POST, OPTIONS, PUT, PATCH, DELETE');
+
+   // Request headers you wish to allow
+   res.setHeader('Access-Control-Allow-Headers', 'X-Requested-With,content-type');
+
+   // Set to true if you need the website to include cookies in the requests sent
+   // to the API (e.g. in case you use sessions)
+   res.setHeader('Access-Control-Allow-Credentials', true);
+
+   // Pass to next layer of middleware
+   next();
   })
 }
 bootstrap();
